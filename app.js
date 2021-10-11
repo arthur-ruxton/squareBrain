1f72b6d745ece3ef7f40af0e9513a900ed3d78d3
eddie ruxton <ae.ruxton@gmail.com>
2021-10-11 13:47:32 +0100

    start button now triggers maze-generation &  player movement is now enabled

---
+++ app.js
@@ -0,0 +1,70 @@
+// initialize game
+let newMaze = new Maze(600, 15, 15);
+// let complete = document.querySelector(".complete");
+
+// start button triggers maze-gen functionality
+document.querySelector(".start-button").addEventListener('click', generateGame)
+// DOM listens for keydown events, call playerMoves
+document.addEventListener("keydown", playMoves);
+
+function generateGame() {
+    newMaze.setup();
+    newMaze.draw();
+   }
+
+function playMoves(e) {
+//if generationComplete is true
+  if (!generationComplete) return;
+// then keyPressed = the keydown event key
+  let keyPressed = e.key;
+// locate the current position assign to variables for future manipulation
+  let row = currentCell.rowNum;
+  let col = currentCell.colNum;
+
+// One case for each of the four arrow keys
+  switch (keyPressed) {
+    case "ArrowUp":
+// check if relevant wall is false before allowing player to pass through
+      if (currentCell.walls.topWall === false) {
+// alter the position of currentCell and highlight only the relevant cell as player moves
+        let next = newMaze.grid[row - 1][col];
+        currentCell = next;
+        newMaze.draw();
+        currentCell.highlight(newMaze.columns);
+      }
+      break;
+
+    case "ArrowRight":
+// check if relevant wall is false before allowing player to pass through
+      if (currentCell.walls.rightWall === false) {
+// alter the position of currentCell and highlight only the relevant cell as player moves
+        let next = newMaze.grid[row][col + 1];
+        currentCell = next;
+        newMaze.draw();
+        currentCell.highlight(newMaze.columns);
+      }
+      break;
+
+    case "ArrowDown":
+// check if relevant wall is false before allowing player to pass through
+      if (currentCell.walls.bottomWall === false) {
+// alter the position of currentCell and highlight only the relevant cell as player moves
+        let next = newMaze.grid[row + 1][col];
+        currentCell = next;
+        newMaze.draw();
+        currentCell.highlight(newMaze.columns);
+      }
+      break;
+
+    case "ArrowLeft":
+// check if relevant wall is false before allowing player to pass through
+      if (currentCell.walls.leftWall === false) {
+// alter the position of currentCell and highlight only the relevant cell as player moves
+        let next = newMaze.grid[row][col - 1];
+        currentCell = next;
+        newMaze.draw();
+        currentCell.highlight(newMaze.columns);
+      }
+      break;
+  }
+}
